8f28e3dbef631e0b60f4ae5dc92b9e2ea2fa55ac
MeiEsparaz <esparaz.marc.andrei@iisgalvanimi.edu.it>
2020-12-16 11:55:07 +0000

    5.d

--- Prep-Verifica/src/app/app-routing.module.ts
+++ Prep-Verifica/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemComponent } from './item/item.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'item/:id', component: ItemComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
